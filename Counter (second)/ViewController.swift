bf92e8a204ee25bfdb94706097d57a0655c097ed
Andrey Bigalinov <greengiovany@yandex.ru>
2023-08-18 11:21:29 +0700

    Решение базовой задачи по спринту 3

--- Counter (second)/ViewController.swift
+++ Counter (second)/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    private var countStore: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func countAdd(_ sender: Any) {
+        countStore += 1
+        countLabel.text = String(countStore)
+    }
+    
 
 }
 
